48227106a045a03306cf4fc11bc7f1caf876f2e2
levosos <razhaleva@gmail.com>
2019-09-04 23:13:44 +0300

    Added 'Interval' enum
    values represent the corresponding # of semitones of each interval

---
+++ interval.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace midiate
+{
+
+enum class Interval
+{
+    // the values are the corresponding numbers of semitones
+
+    P1 = 0,
+    m2 = 1,
+    M2 = 2,
+    m3 = 3,
+    M3 = 4,
+    P4 = 5,
+    A4 = 6,
+    d5 = 6,
+    P5 = 7,
+    A5 = 8,
+    m6 = 8,
+    M6 = 9,
+    m7 = 10,
+    M7 = 11,
+    P8 = 12,
+};
+
+} // midiate
