72ecb337e2aea560328379a3ed501660ddfc7b2e
levosos <razhaleva@gmail.com>
2019-10-29 19:39:26 +0200

    Freeing up some dynamic memory
    - using less global variables for generating scales
    - this saves about ~50 bytes

--- scale.cpp
+++ scale.cpp
@@ -8,24 +8,6 @@ namespace midiate
 namespace
 {
 
-struct Description
-{
-    struct Triad
-    {
-        Interval interval;
-        Quality  quality;
-    };
-
-    Triad I;
-    Triad II;
-    Triad III;
-    Triad IV;
-    Triad V;
-    Triad VI;
-    Triad VII;
-    Triad VIII;
-};
-
 #define ASSERT(mode, expected) static_assert(static_cast<int>(mode) == (expected), "Expected midiate::" #mode " to be equal to " #expected);
 
 ASSERT(Mode::Ionian,     0);
@@ -36,102 +18,40 @@ ASSERT(Mode::Mixolydian, 4);
 ASSERT(Mode::Aeolian,    5);
 ASSERT(Mode::Locrian,    6);
 
-static const Description __descriptions[] =
+static const Interval __intervals[][8] =
     {
-        {
-            // Ionian
-            { Interval::P1, Quality::Major      },
-            { Interval::M2, Quality::Minor      },
-            { Interval::M3, Quality::Minor      },
-            { Interval::P4, Quality::Major      },
-            { Interval::P5, Quality::Major      },
-            { Interval::M6, Quality::Minor      },
-            { Interval::M7, Quality::Diminished },
-            { Interval::P8, Quality::Major      },
-        },
-        {
-            // Dorian
-            { Interval::P1, Quality::Minor      },
-            { Interval::M2, Quality::Minor      },
-            { Interval::m3, Quality::Major      },
-            { Interval::P4, Quality::Major      },
-            { Interval::P5, Quality::Minor      },
-            { Interval::M6, Quality::Diminished },
-            { Interval::m7, Quality::Major      },
-            { Interval::P8, Quality::Minor      },
-        },
-        {
-            // Phrygian
-            { Interval::P1, Quality::Minor      },
-            { Interval::m2, Quality::Major      },
-            { Interval::m3, Quality::Major      },
-            { Interval::P4, Quality::Minor      },
-            { Interval::P5, Quality::Diminished },
-            { Interval::m6, Quality::Major      },
-            { Interval::m7, Quality::Minor      },
-            { Interval::P8, Quality::Minor      },
-        },
-        {
-            // Lydian
-            { Interval::P1, Quality::Major      },
-            { Interval::M2, Quality::Major      },
-            { Interval::M3, Quality::Minor      },
-            { Interval::A4, Quality::Diminished },
-            { Interval::P5, Quality::Major      },
-            { Interval::M6, Quality::Minor      },
-            { Interval::M7, Quality::Minor      },
-            { Interval::P8, Quality::Major      },
-        },
-        {
-            // Mixolydian
-            { Interval::P1, Quality::Major      },
-            { Interval::M2, Quality::Minor      },
-            { Interval::M3, Quality::Diminished },
-            { Interval::P4, Quality::Major      },
-            { Interval::P5, Quality::Minor      },
-            { Interval::M6, Quality::Minor      },
-            { Interval::m7, Quality::Major      },
-            { Interval::P8, Quality::Major      },
-        },
-        {
-            // Aeolian
-            { Interval::P1, Quality::Minor      },
-            { Interval::M2, Quality::Diminished },
-            { Interval::m3, Quality::Major      },
-            { Interval::P4, Quality::Minor      },
-            { Interval::P5, Quality::Minor      },
-            { Interval::m6, Quality::Major      },
-            { Interval::m7, Quality::Major      },
-            { Interval::P8, Quality::Minor      },
-        },
-        {
-            // Locrian
-            { Interval::P1, Quality::Diminished },
-            { Interval::m2, Quality::Major      },
-            { Interval::m3, Quality::Minor      },
-            { Interval::P4, Quality::Minor      },
-            { Interval::d5, Quality::Major      },
-            { Interval::m6, Quality::Major      },
-            { Interval::m7, Quality::Minor      },
-            { Interval::P8, Quality::Diminished },
-        },
+        { Interval::P1, Interval::M2, Interval::M3, Interval::P4, Interval::P5, Interval::M6, Interval::M7, Interval::P8 }, // Ionian
+        { Interval::P1, Interval::M2, Interval::m3, Interval::P4, Interval::P5, Interval::M6, Interval::m7, Interval::P8 }, // Dorian
+        { Interval::P1, Interval::m2, Interval::m3, Interval::P4, Interval::P5, Interval::m6, Interval::m7, Interval::P8 }, // Phrygian
+        { Interval::P1, Interval::M2, Interval::M3, Interval::A4, Interval::P5, Interval::M6, Interval::M7, Interval::P8 }, // Lydian
+        { Interval::P1, Interval::M2, Interval::M3, Interval::P4, Interval::P5, Interval::M6, Interval::m7, Interval::P8 }, // Mixolydian
+        { Interval::P1, Interval::M2, Interval::m3, Interval::P4, Interval::P5, Interval::m6, Interval::m7, Interval::P8 }, // Aeolian
+        { Interval::P1, Interval::m2, Interval::m3, Interval::P4, Interval::d5, Interval::m6, Interval::m7, Interval::P8 }, // Locrian
     };
 
-static_assert(sizeof(__descriptions) / sizeof(Description) == 7, "Expected 7 modes to be declared");
+static_assert(sizeof(__intervals) / sizeof(__intervals[0]) == 7, "Expected 7 modes to be declared");
 
-#define DESCRIPTION(mode) (__descriptions[static_cast<int>(mode)])
+#define INTERVAL(mode, degree) (__intervals[static_cast<int>(mode)][degree - 1])
+
+static const Quality __qualities[] = { Quality::Major, Quality::Minor, Quality::Minor, Quality::Major, Quality::Major, Quality::Minor, Quality::Diminished };
+
+static_assert(sizeof(__qualities) / sizeof(__qualities[0]) == 7, "Expected 7 qualities to be declared");
+
+#define QUALITY(mode, degree) (__qualities[((static_cast<int>(degree) - 1) + static_cast<int>(mode)) % (sizeof(__qualities) / sizeof(__qualities[0]))])
+
+#define TRIAD(root, mode, degree) Triad(root + INTERVAL(mode, degree), QUALITY(mode, degree))
 
 } //
 
 Scale::Scale(Pitch root, Mode mode) :
-    I   (root + DESCRIPTION(mode).I.interval,    DESCRIPTION(mode).I.quality),
-    II  (root + DESCRIPTION(mode).II.interval,   DESCRIPTION(mode).II.quality),
-    III (root + DESCRIPTION(mode).III.interval,  DESCRIPTION(mode).III.quality),
-    IV  (root + DESCRIPTION(mode).IV.interval,   DESCRIPTION(mode).IV.quality),
-    V   (root + DESCRIPTION(mode).V.interval,    DESCRIPTION(mode).V.quality),
-    VI  (root + DESCRIPTION(mode).VI.interval,   DESCRIPTION(mode).VI.quality),
-    VII (root + DESCRIPTION(mode).VII.interval,  DESCRIPTION(mode).VII.quality),
-    VIII(root + DESCRIPTION(mode).VIII.interval, DESCRIPTION(mode).VIII.quality)
+    I   (TRIAD(root, mode, 1)),
+    II  (TRIAD(root, mode, 2)),
+    III (TRIAD(root, mode, 3)),
+    IV  (TRIAD(root, mode, 4)),
+    V   (TRIAD(root, mode, 5)),
+    VI  (TRIAD(root, mode, 6)),
+    VII (TRIAD(root, mode, 7)),
+    VIII(TRIAD(root, mode, 8))
 {}
 
 const Triad & Scale::degree(int i) const
