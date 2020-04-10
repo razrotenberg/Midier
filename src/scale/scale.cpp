2806ee9c8c175eca5470759e33482f9f93ae3cd8
levosos <razhaleva@gmail.com>
2020-04-10 11:37:13 +0300

    Using PROGMEM for constants in scale.cpp

--- src/scale/scale.cpp
+++ src/scale/scale.cpp
@@ -1,11 +1,10 @@
 #include "scale.h"
 
+#include <Arduino.h>
+
 namespace midier
 {
 namespace scale
-{
-
-namespace
 {
 
 #define ASSERT(mode, expected) static_assert(static_cast<int>(mode) == (expected), "Expected midier::" #mode " to be equal to " #expected);
@@ -18,33 +17,43 @@ ASSERT(Mode::Mixolydian, 4);
 ASSERT(Mode::Aeolian,    5);
 ASSERT(Mode::Locrian,    6);
 
-static const Interval __intervals[][8] =
-    {
-        { Interval::P1, Interval::M2, Interval::M3, Interval::P4, Interval::P5, Interval::M6, Interval::M7, Interval::P8 }, // Ionian
-        { Interval::P1, Interval::M2, Interval::m3, Interval::P4, Interval::P5, Interval::M6, Interval::m7, Interval::P8 }, // Dorian
-        { Interval::P1, Interval::m2, Interval::m3, Interval::P4, Interval::P5, Interval::m6, Interval::m7, Interval::P8 }, // Phrygian
-        { Interval::P1, Interval::M2, Interval::M3, Interval::A4, Interval::P5, Interval::M6, Interval::M7, Interval::P8 }, // Lydian
-        { Interval::P1, Interval::M2, Interval::M3, Interval::P4, Interval::P5, Interval::M6, Interval::m7, Interval::P8 }, // Mixolydian
-        { Interval::P1, Interval::M2, Interval::m3, Interval::P4, Interval::P5, Interval::m6, Interval::m7, Interval::P8 }, // Aeolian
-        { Interval::P1, Interval::m2, Interval::m3, Interval::P4, Interval::d5, Interval::m6, Interval::m7, Interval::P8 }, // Locrian
-    };
-
-static_assert(sizeof(__intervals) / sizeof(__intervals[0]) == 7, "Expected 7 modes to be declared");
-
-static const Quality __qualities[] = { Quality::maj7, Quality::m7, Quality::m7, Quality::maj7, Quality::dom7, Quality::m7, Quality::m7b5 };
-
-static_assert(sizeof(__qualities) / sizeof(__qualities[0]) == 7, "Expected 7 qualities to be declared");
-
-} //
-
 Interval interval(Mode mode, Degree degree)
 {
-    return __intervals[(unsigned)mode][degree - 1];
+    static Interval const PROGMEM __ionian     [] = { Interval::P1, Interval::M2, Interval::M3, Interval::P4, Interval::P5, Interval::M6, Interval::M7, Interval::P8 };
+    static Interval const PROGMEM __dorian     [] = { Interval::P1, Interval::M2, Interval::m3, Interval::P4, Interval::P5, Interval::M6, Interval::m7, Interval::P8 };
+    static Interval const PROGMEM __phrygian   [] = { Interval::P1, Interval::m2, Interval::m3, Interval::P4, Interval::P5, Interval::m6, Interval::m7, Interval::P8 };
+    static Interval const PROGMEM __lydian     [] = { Interval::P1, Interval::M2, Interval::M3, Interval::A4, Interval::P5, Interval::M6, Interval::M7, Interval::P8 };
+    static Interval const PROGMEM __mixolydian [] = { Interval::P1, Interval::M2, Interval::M3, Interval::P4, Interval::P5, Interval::M6, Interval::m7, Interval::P8 };
+    static Interval const PROGMEM __aeolian    [] = { Interval::P1, Interval::M2, Interval::m3, Interval::P4, Interval::P5, Interval::m6, Interval::m7, Interval::P8 };
+    static Interval const PROGMEM __locrian    [] = { Interval::P1, Interval::m2, Interval::m3, Interval::P4, Interval::d5, Interval::m6, Interval::m7, Interval::P8 };
+
+    static Interval const * const __all[] PROGMEM =
+        {
+            __ionian,
+            __dorian,
+            __phrygian,
+            __lydian,
+            __mixolydian,
+            __aeolian,
+            __locrian,
+        };
+
+    static_assert(sizeof(__all) / sizeof(__all[0]) == (unsigned)Mode::Count, "Unexpected number of modes declared");
+    static_assert(sizeof(Interval) == 1, "Unexpected size of 'Interval'");
+
+    return (Interval)pgm_read_byte(pgm_read_ptr(__all + (unsigned)mode) + (degree - 1));
 }
 
 Quality quality(Mode mode, Degree degree)
 {
-    return __qualities[(degree - 1 + (unsigned)mode) % (sizeof(__qualities) / sizeof(__qualities[0]))];
+    static Quality const PROGMEM __qualities[] = { Quality::maj7, Quality::m7, Quality::m7, Quality::maj7, Quality::dom7, Quality::m7, Quality::m7b5 };
+
+    constexpr auto __count = sizeof(__qualities) / sizeof(__qualities[0]);
+
+    static_assert(__count == 7, "Expected 7 qualities to be declared");
+    static_assert(sizeof(Quality) == 1, "Unexpected size of 'Quality'");
+
+    return (Quality)pgm_read_byte(__qualities + ((degree - 1 + (unsigned)mode) % __count));
 }
 
 } // scale
